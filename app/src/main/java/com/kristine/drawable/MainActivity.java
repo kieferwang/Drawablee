44ee53b5ea56a073e2cbf3de240d2b7d980c72ab
kristine <1053385617@qq.com>
2016-02-04 14:36:39 +0800

    控件外观

--- app/src/main/java/com/kristine/drawable/MainActivity.java
+++ app/src/main/java/com/kristine/drawable/MainActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    private int nale;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
